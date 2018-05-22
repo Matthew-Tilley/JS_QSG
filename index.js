75e3c6953c7db279648b034feddb94c7405f5c0d
Matt Tilley <mtilley255@gmail.com>
2018-05-22 09:44:11 -0500

    Location 283.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-document.write("This is another thing to indicate JavaScript is beginning to make sense.");
+let name = "Martin";
+
+
+
+//document.write("This is another thing to indicate JavaScript is beginning to make sense.");
 
 //alert("JavaScript is starting to make sense!");
